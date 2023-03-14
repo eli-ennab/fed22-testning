a23549a9400fb5cf431b54e9b05475f885f4ec44
Eli Ennab <elinaennab@gmail.com>
2023-03-14 13:25:42 +0100

    subtract numbers and suite tests with describe

--- 01-unit-testing/src/sum.ts
+++ 01-unit-testing/src/sum.ts
@@ -1,19 +1,26 @@
 /**
- * Add a and b together and return sum
+ * Add a and b together and return sum..
  */
 /*
-export const sum = (a: number, b: number) => {
+export const add = (a: number, b: number) => {
 	return a + b
 }
 */
 
 // with a rest syntax (that takes all parameters we send in and put all of them in an array)
-export const sum = (...numbers: number[]) => {
+export const add = (...numbers: number[]) => {
 
-	// const sum = numbers.reduce((acc, num) => {
+	// const add = numbers.reduce((acc, num) => {
 	// 	return acc + num
 	// }, 0)
 	// return sum
 
 	return numbers.reduce((acc, num) => acc + num, 0)
+}
+
+/**
+ * Subract numbers from each other and return sum
+ */
+export const sub = (initialValue: number, ...numbers: number[]) => {
+	return numbers.reduce((acc, num) => acc - num, initialValue)
 }
