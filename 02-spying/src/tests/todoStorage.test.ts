92b11243da4f6c00be5be04e2964ed6af55a167c
Eli Ennab <elinaennab@gmail.com>
2023-03-16 12:04:34 +0100

    test if it can save a todo and clear local storage after each

--- 02-spying/src/tests/todoStorage.test.ts
+++ 02-spying/src/tests/todoStorage.test.ts
@@ -1,8 +1,15 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it } from 'vitest'
 import mockLocalStorage from '../mocks/mockedLocalStorage'
+import { Todo } from '../types/Todo'
 import { getTodos, saveTodos } from '../utils/todoStorage'
 
 global.localStorage = mockLocalStorage()
+
+const TODO: Todo = {
+	id: 1,
+	title: 'My first todo',
+	completed: false,
+}
 
 describe('get todos', () => {
 	it('returns empty list of todos', () => {
@@ -13,6 +20,24 @@ describe('get todos', () => {
 })
 
 describe('save todos', () => {
-	it.todo('can save a todo', () => {})
-	it.todo('can save a todo and then get it', () => {})
+
+	afterEach(() => {
+		global.localStorage.clear()
+	})
+
+	it('can save a todo', () => {
+		const result = saveTodos([TODO])
+
+		expect(result.success).toBe(true)
+	})
+
+	// it('can save a todo and then get it', () => {
+	// 	const result = saveTodos([TODO])
+
+	// 	expect(result.success).toBe(true)
+
+	// 	const todos = getTodos()
+
+	// 	expect(todos.length).toBe(1)
+	// })
 })
