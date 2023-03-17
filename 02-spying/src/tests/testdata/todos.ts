a83e36d88aca0a3088430125d36d795a51692dd2
Eli Ennab <elinaennab@gmail.com>
2023-03-17 11:33:18 +0100

    created dummy todos

---
+++ 02-spying/src/tests/testdata/todos.ts
@@ -0,0 +1,21 @@
+import { Todo } from "../../types/Todo"
+
+const todos: Todo[] = [
+	{
+		id: 1,
+		title: 'My first todo',
+		completed: false
+	},
+	{
+		id: 2,
+		title: 'My second todo',
+		completed: true
+	},
+	{
+		id: 3,
+		title: 'My third todo',
+		completed: false
+	}
+]
+
+export default todos
