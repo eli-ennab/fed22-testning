6c98574a6fddbffbeb746bb84b11861e7f0fc8e1
Eli Ennab <elinaennab@gmail.com>
2023-03-28 14:27:38 +0200

    created context for initial state and for create todo

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -1,49 +1,57 @@
-import { should } from "chai"
-
 describe('Todos', () => {
 
 	beforeEach(() => {
 		cy.visit('/')
 	})
 
-	it('should see at least one todo', { defaultCommandTimeout: 6000 }, () => {
-		cy.get('#todos')
-			.find('li')
-			.should('have.length.at.least', 1)
+	context('initial state', () => {
+		it('should see at least one todo', { defaultCommandTimeout: 6000 }, () => {
+			cy.get('#todos')
+				.find('li')
+				.should('have.length.at.least', 1)
+		})
+
+		it('should not show error dialog', () => {
+			cy.get('#error')
+				.should('not.be.visible')
+		})
 	})
 
-	it('create todo form should be empty', () => {
-		cy.get('#new-todo-title')
-			.should('be.empty')
-			.should('contain', '')
+	context('create todo', () => {
+		it('create todo form should be empty', () => {
+			cy.get('#new-todo-title')
+				.should('have.value', '')
+		})
+
+		it('cannot create a todo without a title', () => {
+			// cy.get('#new-todo-title').type(`{enter}`)
+			cy.get('[type="submit"]').click()
+
+			cy.get('#error')
+				.should('be.visible')
+				.contains('Title cannot be empty')
+		})
+
+		it('can create a new todo (and see it in the list)', () => {
+			const todoTitle = 'Learn E2E Testing'
+			cy.get('#new-todo-title').type(`${todoTitle}{enter}`)
+
+			cy.get('#todos')
+				.find('li')
+				.last()
+				.contains(todoTitle)
+
+			cy.get('#new-todo-title').should('have.value', '')
+		})
+
+		it('can type in the create todo form and then reset the form', () => {
+			const typingTodo = 'Learn E2E Tes..'
+			cy.get('#new-todo-title').type(`${typingTodo}`)
+
+			cy.get('[type="reset"]').click()
+
+			cy.get('#new-todo-title')
+				.should('have.value', '')
+		})
 	})
-
-	it('can not create a todo without a title', () => {
-		const emptyTodoTitle = ''
-		cy.get('#new-todo-title').type(`${emptyTodoTitle}{enter}`)
-
-		cy.get('#error')
-			.should('contain', 'Title cannot be empty')
-	})
-
-	it('can create a new todo (and see it in the list)', () => {
-		const todoTitle = 'Learn E2E Testing'
-		cy.get('#new-todo-title').type(`${todoTitle}{enter}`)
-
-		cy.get('#todos li .todo-title')
-			.last()
-			.should('have.text', todoTitle)
-	})
-
-	it('can type in the create todo form and then reset the form', () => {
-		const typingTodo = 'Learn E2E Tes..'
-		cy.get('#new-todo-title').type(`${typingTodo}`)
-
-		cy.contains('Reset').click()
-
-		cy.get('#new-todo-title')
-			.should('not.contain', typingTodo)
-			.should('contain', '')
-	})
-
 })
